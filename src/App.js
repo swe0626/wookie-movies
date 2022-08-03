904665e783735f4f020175c61270823d572f9588
ReactMad <urosjevtic9.it@gmail.com>
2022-08-03 10:18:48 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App flex">
+      <div> 1 block </div>
+      <div className='text-black'> 2 block </div>
+    </div>
+  );
+}
+
+export default App;
